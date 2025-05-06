d0112cb443c91bf8fdddd7fac1ecb09d852096bb
lakmini77 <98383006+lakmini77@users.noreply.github.com>
2025-05-06 18:42:56 +0530

    Update signup.php
    Signup with was with some errors completed

--- signup.php
+++ signup.php
@@ -1,9 +1,7 @@
 <?php include './components/header.php'; ?>
+<?php include './components/top-menu.php'; ?>
 
-    <div class="ui container">
 
-        <!-- Top Navigation Bar -->
-        <?php include './components/top-menu.php'; ?>
 
         <!-- BODY Content -->
         <div class="ui centered grid">
